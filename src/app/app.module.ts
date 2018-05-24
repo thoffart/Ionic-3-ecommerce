6addc341bdb7ed236c458ecbd09b53959683cc87
thoffart <thiagohoffart321920@gmail.com>
2018-05-24 10:55:30 -0300

    pages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { QuotesPageModule } from "./../pages/quotes/quotes.module";
+import { QuotePageModule } from "./../pages/quote/quote.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { FavoritesPage } from "../pages/favorites/favorites";
+import { LibraryPageModule } from "../pages/library/library.module";
+import { SettingsPageModule } from "../pages/settings/settings.module";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, FavoritesPage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    LibraryPageModule,
+    QuotePageModule,
+    QuotesPageModule,
+    SettingsPageModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, FavoritesPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
